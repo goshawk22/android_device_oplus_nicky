577763ef5110cdae3c552b7772f960df55bccc4f
Luofan Chen <clfbbn@gmail.com>
2022-12-16 22:56:41 +0530

    nicky: ship android.system.keystore2
    Fixes: Could not find 'aidl/android.system.keystore2.IKeystoreService/default'
           for ctl.interface_start from pid: 617 (/system/bin/servicemanager)

--- device.mk
+++ device.mk
@@ -243,6 +243,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# Keystore
+PRODUCT_PACKAGES += \
+    android.system.keystore2
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.nicky
